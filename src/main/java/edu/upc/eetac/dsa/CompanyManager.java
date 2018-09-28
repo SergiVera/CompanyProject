8e13a76180bedfcab8cceaf0322871fb024ef9f9
SergiVera <sergitus1998@gmail.com>
2018-09-28 11:33:29 +0200

    add interface CompanyManager

---
+++ src/main/java/edu/upc/eetac/dsa/CompanyManager.java
@@ -0,0 +1,15 @@
+package edu.upc.eetac.dsa;
+
+import java.util.Date;
+import java.util.List;
+
+public interface CompanyManager {
+
+    public void addCompany(String companyName, String description);
+    public void addEmployee(String name, String username, Date birthday, double salary, String companyName) throws MyException;
+    public List<Employee> findAllEmployeesOrderedByName();
+    public List<Employee> findAllEmployeesOrderedBySalary();
+    public List<Employee> employees(String company);
+    public List<Company> findAllCompanies();
+    
+}
